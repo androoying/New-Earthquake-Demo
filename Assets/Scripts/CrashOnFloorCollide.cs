d55ff4c2a82b5eb6277759efd6156836200bb296
Andrew Ying <andrew@andrewying.org>
2019-03-06 16:49:59 -0800

    Nomenclature
    Renaming scenes and scripts for clarity.

--- Assets/Scripts/CrashOnFloorCollide.cs
+++ Assets/Scripts/CrashOnFloorCollide.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Crash : MonoBehaviour
+public class CrashOnFloorCollide : MonoBehaviour
 {
     public AmbisonicsSystem ambSys;
     public string file;
